96f7f827f63b9fe98ec6ad023596b00e1ecb5ffc
Ganesh Kolekar <kolekarganesh5151@gmail.com>
2024-03-31 23:35:34 +0530

    push code to remote origin

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const db = require("./src/models");
 const route=require("./src/routes/index.route")
 const port=8000;
 
 
+
+
 const app=express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+//routes
 app.use('/api',route);
 app.get('/',(req,res)=>{
     res.send("welcome to app")
